c27d5b4b76cf243e00b316203dac1d0df35826d7
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-02-28 10:57:33 -0600

    Alcance o Encapsulamiento

---
+++ alcance.php
@@ -0,0 +1,19 @@
+<?php
+
+class User
+{
+  // public  
+  // protected 
+  // private
+  public const PAGINATE = 25;
+
+  public $username;
+  // protected $username;
+  // private $username;
+
+  public function getUsername()
+  {
+    // ...
+  }
+
+}
