ca84a12fbb079ace8d3880d3c64c1eb1033da70d
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-02-28 09:41:12 -0600

    Intro clases y objetos

---
+++ clases.php
@@ -0,0 +1,25 @@
+<?php
+
+class Person
+{
+  public function greet()
+  {
+    return "Hola $this->name";
+  }
+
+}
+
+class User
+{
+  public $type;
+}
+
+class Admin extends Person
+{
+  public $name = 'Administrador';
+}
+
+$user = new User;
+$user->type = new Admin;
+echo $user->type->greet();
+
